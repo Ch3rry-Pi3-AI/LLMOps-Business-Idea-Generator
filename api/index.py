63c1b1ac34ebaea63678c6ffca9af8e536d49580
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-10-25 00:11:32 +0100

    resolving problems

--- api/index.py
+++ api/index.py
@@ -7,6 +7,6 @@ app = FastAPI()
 @app.get("/api", response_class=PlainTextResponse)
 def idea():
     client = OpenAI()
-    prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
-    response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
+    prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=prompt)
     return response.choices[0].message.content
